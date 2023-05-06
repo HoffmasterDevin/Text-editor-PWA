a2ff8242633b7b869a331937dfaa55a3b28aea22
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-06 08:13:29 -0500

    updated database JS, and added plugin logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  export const putDb = async function (content) {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ value: content });
+    const result = await request;
+  };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  export const getDb = async function () {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+  
+    if (result.length == 0) { // Returns false if no results are found
+      console.log('No previous edits - show JATE header');
+      return false;
+    } else { // Returns the last item if results are found
+      console.log('Previous edit found - show results');
+      return result[result.length - 1].value;
+    }
+  };
 
 initdb();
